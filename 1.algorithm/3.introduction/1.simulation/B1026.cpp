e757285a9cfb345ab5b7f11c2053fbce3a3eb062
zhouyuhao <zhouyuhaoqin@qq.com>
2025-02-24 15:01:54 +0800

    Introduction - Simulation

--- 1.algorithm/3.introduction/1.simulation/B1026.cpp
+++ 1.algorithm/3.introduction/1.simulation/B1026.cpp
@@ -7,10 +7,10 @@
  */
 
 /*
-  @pintia psid=994805260223102976 pid=994805295203598336 compiler=GXX
-  ProblemSet: PAT (Basic Level) Practice （中文）
-  Title: 1026 程序运行时间
-  https://pintia.cn/problem-sets/994805260223102976/exam/problems/type/7?problemSetProblemId=994805295203598336
+    @pintia psid=994805260223102976 pid=994805295203598336 compiler=GXX
+    ProblemSet: PAT (Basic Level) Practice （中文）
+    Title: 1026 程序运行时间
+    https://pintia.cn/problem-sets/994805260223102976/exam/problems/type/7?problemSetProblemId=994805295203598336
 */
 
 // @pintia code=start
