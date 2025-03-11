cd97e424552ab2e118a4bc62e42f15ee7b294177
zhouyuhao <zhouyuhaoqin@qq.com>
2025-03-11 08:38:03 +0800

    Math - Rational Arithmetic

---
+++ 1.algorithm/5.math/3.rational arithmetic/A1088-2.cpp
@@ -0,0 +1,64 @@
+/*
+ *	author:		zhouyuhao
+ *	created:	2024-05-02 20:10:46
+ *	modified:	2024-05-02 20:16:46
+ *	item:		Programming Ability Test
+ *	site:		226, Harbin
+ */
+
+/*
+  @pintia psid=994805342720868352 pid=994805378443755520 compiler=GXX
+  ProblemSet: PAT (Advanced Level) Practice
+  Title: 1088 Rational Arithmetic
+  https://pintia.cn/problem-sets/994805342720868352/exam/problems/type/7?problemSetProblemId=994805378443755520
+*/
+
+// @pintia code=start
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+ll gcd(ll a, ll b) { // greatest common divisor
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+void print(ll a, ll b) { // print(fraction) -> print(numerator, denominator)
+    if (a * b == 0) {
+        !b ? cout << "Inf" : cout << "0";
+        return;
+    }
+    if (b < 0) {
+        a = -a, b = -b;
+    }
+    int g = gcd(abs(a), b);
+    a /= g, b /= g;
+    if (a < 0) {
+        cout << "(";
+    }
+    if (b == 1) {
+        cout << a;
+    } else if (abs(a) > b) {
+        cout << a / b << " " << abs(a) % b << "/" << b;
+    } else {
+        cout << a << "/" << b;
+    }
+    if (a < 0) {
+        cout << ")";
+    }
+}
+
+int main(int argc, char const *argv[]) {
+
+    ll a, b, c, d;
+    int unused __attribute__((unused)) = 0;
+    unused = scanf("%lld/%lld %lld/%lld", &a, &b, &c, &d);
+    print(a, b), cout << " + ", print(c, d), cout << " = ", print(a * d + b * c, b * d), cout << "\n";
+    print(a, b), cout << " - ", print(c, d), cout << " = ", print(a * d - b * c, b * d), cout << "\n";
+    print(a, b), cout << " * ", print(c, d), cout << " = ", print(a * c, b * d), cout << "\n";
+    print(a, b), cout << " / ", print(c, d), cout << " = ", print(a * d, b * c), cout << "\n";
+
+    return 0;
+}
+// @pintia code=end
---
+++ 1.algorithm/5.math/3.rational arithmetic/B1034-1.cpp
@@ -0,0 +1,97 @@
+/*
+ *	author:		zhouyuhao
+ *	created:	2023-04-11 21:33:00
+ *	modified:	2023-04-11 23:18:19
+ *	item:		Programming Ability Test
+ *	site:		Shahu
+ */
+
+/*
+  @pintia psid=994805260223102976 pid=994805287624491008 compiler=GXX
+  ProblemSet: PAT (Basic Level) Practice （中文）
+  Title: 1034 有理数四则运算
+  https://pintia.cn/problem-sets/994805260223102976/exam/problems/type/7?problemSetProblemId=994805287624491008
+*/
+
+// @pintia code=start
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+ll gcd(ll a, ll b) { // greatest common divisor
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+struct fra { // fraction
+    ll num, den; // numerator, denominator
+};
+
+fra reduct(fra r) { // reduction
+    if (r.den < 0) {
+        r.num = -r.num, r.den = -r.den;
+    }
+    if (r.num == 0) {
+        r.den = 1;
+    } else {
+        int g = gcd(abs(r.num), r.den);
+        r.num /= g, r.den /= g;
+    }
+    return r;
+}
+
+fra add(fra f1, fra f2) { // addition
+    fra r = {f1.num * f2.den + f2.num * f1.den, f1.den * f2.den};
+    return reduct(r);
+}
+
+fra subtract(fra f1, fra f2) { // subtraction
+    fra r = {f1.num * f2.den - f2.num * f1.den, f1.den * f2.den};
+    return reduct(r);
+}
+
+fra multiply(fra f1, fra f2) { // multiplication
+    fra r = {f1.num * f2.num, f1.den * f2.den};
+    return reduct(r);
+}
+
+fra divide(fra f1, fra f2) { // division
+    fra r = {f1.num * f2.den, f1.den * f2.num};
+    return reduct(r);
+}
+
+void print(fra r) { // print
+    r = reduct(r);
+    if (r.num < 0) {
+        cout << "(";
+    }
+    if (r.den == 1) {
+        cout << r.num;
+    } else if (abs(r.num) > r.den) {
+        cout << r.num / r.den << " " << abs(r.num) % r.den << "/" << r.den;
+    } else {
+        cout << r.num << "/" << r.den;
+    }
+    if (r.num < 0) {
+        cout << ")";
+    }
+}
+
+void printInf() {
+    cout << "Inf";
+}
+
+int main(int argc, char const *argv[]) {
+
+    fra a, b;
+    int unused __attribute__((unused)) = 0;
+    unused = scanf("%lld/%lld %lld/%lld", &a.num, &a.den, &b.num, &b.den);
+    print(a), cout << " + ", print(b), cout << " = ", print(add(a, b)), cout << "\n";
+    print(a), cout << " - ", print(b), cout << " = ", print(subtract(a, b)), cout << "\n";
+    print(a), cout << " * ", print(b), cout << " = ", print(multiply(a, b)), cout << "\n";
+    print(a), cout << " / ", print(b), cout << " = ", !b.num ? printInf() : print(divide(a, b)), cout << "\n";
+
+    return 0;
+}
+// @pintia code=end
