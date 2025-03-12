1cf501b2e617b73d7a5e75c7fd154b250d1c8545
zhouyuhao <zhouyuhaoqin@qq.com>
2025-03-12 08:25:56 +0800

    Math - Mathematics

---
+++ 1.algorithm/5.math/1.mathematics/A1008-1.cpp
@@ -0,0 +1,44 @@
+/*
+ *	author:		zhouyuhao
+ *	created:	2023-04-11 11:16:27
+ *	modified:	2023-04-11 11:31:57
+ *	item:		Programming Ability Test
+ *	site:		Shahu
+ */
+
+/*
+  @pintia psid=994805342720868352 pid=994805511923286016 compiler=GXX
+  ProblemSet: PAT (Advanced Level) Practice
+  Title: 1008 Elevator
+  https://pintia.cn/problem-sets/994805342720868352/exam/problems/type/7?problemSetProblemId=994805511923286016
+*/
+
+// @pintia code=start
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    int n;
+    cin >> n;
+    vector<int> l(n + 1);
+    for (int i = 0; i < n; i++) {
+        cin >> l[i + 1];
+    }
+    int sum = 0;
+    for (int i = 0; i < (int)l.size() - 1; i++) {
+        int now = l[i], next = l[i + 1];
+        if (next > now) {
+            sum += (next - now) * 6;
+        } else {
+            sum += (now - next) * 4;
+        }
+        sum += 5;
+    }
+    cout << sum << "\n";
+
+    return 0;
+}
+// @pintia code=end
---
+++ 1.algorithm/5.math/1.mathematics/B1003-2.cpp
@@ -0,0 +1,51 @@
+/*
+ *	author:		zhouyuhao
+ *	created:	2024-04-29 10:01:00
+ *	modified:	2024-04-29 10:10:00
+ *	item:		Programming Ability Test
+ *	site:		914, Harbin
+ */
+
+/*
+  @pintia psid=994805260223102976 pid=994805323154440192 compiler=GXX
+  ProblemSet: PAT (Basic Level) Practice （中文）
+  Title: 1003 我要通过！
+  https://pintia.cn/problem-sets/994805260223102976/exam/problems/type/7?problemSetProblemId=994805323154440192
+*/
+
+// @pintia code=start
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    int n;
+    cin >> n;
+    for (int q = 0; q < n; q++) {
+        string s;
+        cin >> s;
+        map<char, int> cnt;
+        int ip = -1, it = -1;
+        for (int i = 0; i < (int)s.size(); i++) {
+            ++cnt[s[i]];
+            if (s[i] == 'P') {
+                ip = i;
+            } else if (s[i] == 'T') {
+                it = i;
+            }
+        }
+        if (cnt['P'] == 1 && cnt['A'] != 0 && cnt['T'] == 1 && cnt.size() == 3) {
+            int la = ip, ma = it - ip, ra = s.size() - it - 1;
+            if (ma > 1 && la * (ma - 1) == ra) {
+                cout << "YES\n";
+                continue;
+            }
+        }
+        cout << "NO\n";
+    }
+
+    return 0;
+}
+// @pintia code=end
