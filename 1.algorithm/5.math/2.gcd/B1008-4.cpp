a25877bb3031aa505011f75185b0cfa7d62076bc
zhouyuhao <zhouyuhaoqin@qq.com>
2025-03-12 08:25:43 +0800

    Math - GCD

---
+++ 1.algorithm/5.math/2.gcd/B1008-4.cpp
@@ -0,0 +1,63 @@
+/*
+ *	author:		zhouyuhao
+ *	created:	2024-05-02 19:29:46
+ *	modified:	2024-05-02 19:41:46
+ *	item:		Programming Ability Test
+ *	site:		226, Harbin
+ */
+
+/*
+  @pintia psid=994805260223102976 pid=994805316250615808 compiler=GXX
+  ProblemSet: PAT (Basic Level) Practice （中文）
+  Title: 1008 数组元素循环右移问题
+  https://pintia.cn/problem-sets/994805260223102976/exam/problems/type/7?problemSetProblemId=994805316250615808
+*/
+
+// @pintia code=start
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int gcd(int a, int b) { // greatest common divisor
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+int main(int argc, char const *argv[]) {
+
+    int n, m;
+    cin >> n >> m;
+    vector<int> d(n);
+    for (int i = 0; i < n; i++) {
+        cin >> d[i];
+    }
+    m %= n;
+    if (m != 0) {
+        // cycle (permutation): a sequence of positions where
+        // elements are moved in a circular manner, forming a closed loop
+        // original: [1, 2, 3, 4, 5, 6]
+        // cycle 1: [5, 2, 1, 4, 3, 6]
+        // cycle 2: [5, 6, 1, 2, 3, 4]
+        int c = gcd(n, m);
+        for (int i = 0; i < c; i++) {
+            int now = i, temp = d[now];
+            do {
+                int next = (now + m) % n;
+                swap(temp, d[next]);
+                // temp = d[now] -> d[next] -> d[next[next]]...
+                now = next;
+            } while (now != i);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << d[i];
+        if (i < n - 1) {
+            cout << " ";
+        } else {
+            cout << "\n";
+        }
+    }
+
+    return 0;
+}
+// @pintia code=end
