65ca0af2cdff5260f237fd0b74e9220bcfb6d725
zhouyuhao <zhouyuhaoqin@qq.com>
2025-04-05 19:43:29 +0800

    Test - A1128-A1131

--- 2.test/A1128-A1131/A1128.cpp
+++ 2.test/A1128-A1131/A1128.cpp
@@ -21,15 +21,20 @@
 using namespace std;
 
 bool isque(vector<int> d) { // judge whether the queen is safe
-    map<int, bool> row, diagonal;
-    for (int i = 0; i < (int)d.size(); i++) {
+    map<int, bool> row, diag1, diag2;
+    for (int i = 1; i < (int)d.size(); i++) {
         if (row[d[i]] == false) {
             row[d[i]] = true;
         } else {
             return false;
         }
-        if (diagonal[d[i] + i] == false) { // diagonal line
-            diagonal[d[i] + i] = true;
+        if (diag1[d[i] + i] == false) { // main diagonal: (row + column) is constant
+            diag1[d[i] + i] = true;
+        } else {
+            return false;
+        }
+        if (diag2[d[i] - i] == false) { // anti diagonal: (row - column) is constant
+            diag2[d[i] - i] = true;
         } else {
             return false;
         }
@@ -44,9 +49,10 @@ int main(int argc, char const *argv[]) {
     for (int i = 0; i < k; i++) {
         int n;
         cin >> n;
-        vector<int> d(n);
-        for (int j = 0; j < n; j++) {
-            cin >> d[j];
+        vector<int> d(n + 1); // all 1-based index
+        // chessboard: left-bottom corner is (1, 1)
+        for (int j = 1; j <= n; j++) { // column
+            cin >> d[j]; // row
         }
         if (isque(d)) {
             cout << "YES\n";
