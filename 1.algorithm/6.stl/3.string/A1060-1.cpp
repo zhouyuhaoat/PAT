cba0d8891201576d74c86eb7f274973245246f84
zhouyuhao <zhouyuhaoqin@qq.com>
2025-04-15 12:39:19 +0800

    STL - String

--- 1.algorithm/6.stl/3.string/A1060-1.cpp
+++ 1.algorithm/6.stl/3.string/A1060-1.cpp
@@ -7,10 +7,10 @@
  */
 
 /*
-  @pintia psid=994805342720868352 pid=994805413520719872 compiler=GXX
-  ProblemSet: PAT (Advanced Level) Practice
-  Title: 1060 Are They Equal
-  https://pintia.cn/problem-sets/994805342720868352/exam/problems/type/7?problemSetProblemId=994805413520719872
+    @pintia psid=994805342720868352 pid=994805413520719872 compiler=GXX
+    ProblemSet: PAT (Advanced Level) Practice
+    Title: 1060 Are They Equal
+    https://pintia.cn/problem-sets/994805342720868352/exam/problems/type/7?problemSetProblemId=994805413520719872
 */
 
 // @pintia code=start
@@ -18,10 +18,7 @@
 
 using namespace std;
 
-string trans(string s, int n) {
-    while (s[0] == '0' && s.size() > 1 && s[1] != '.') { // leading zeros
-        s.erase(0, 1);
-    }
+string convert(string s, int n) {
     int dot = -1, nonZero = -1; // dot, first non-zero
     for (int i = 0; i < (int)s.size() && (dot == -1 || nonZero == -1); i++) {
         if (s[i] == '.') {
@@ -30,26 +27,20 @@ string trans(string s, int n) {
             nonZero = i;
         }
     }
-    int e = 0;
-    string num;
-    if (nonZero != -1) { // not zero
-        // exponent
-        if (nonZero == 0) { // nonZero + ...
-            if (dot != -1) {
-                s.erase(dot, 1); // remove dot for number
-                e = dot;
-            } else {
-                e = s.size();
-            }
-        } else { // 0. + zero + nonZero + ...
-            e = -(nonZero - dot - 1);
-        }
-        // number
-        num = s.substr(nonZero, n);
-    } // else: zero
-    if ((int)num.size() < n) { // trailing zeros
-        num.append(n - num.size(), '0');
+    if (nonZero == -1) { // all zeros
+        return "0." + string(n, '0') + "*10^0";
     }
+    string num; // number
+    for (int i = nonZero; i < (int)s.size(); i++) {
+        if ((int)num.size() < n && isdigit(s[i])) {
+            num.push_back(s[i]);
+        }
+    }
+    num.append(n - num.size(), '0'); // trailing zeros
+    if (dot == -1) dot = s.size(); // dummy dot
+    int e = dot - nonZero > 0 ? dot - nonZero : dot - nonZero + 1; // exponent
+    // dot - nonZero > 0: nonZero ... dot => 0 dot nonZero ...
+    // dot - nonZero < 0: dot 0s nonZero ... => 0 dot nonZero ...
     return "0." + num + "*10^" + to_string(e);
 }
 
@@ -58,7 +49,7 @@ int main(int argc, char const *argv[]) {
     int n;
     string a, b;
     cin >> n >> a >> b;
-    string sa = trans(a, n), sb = trans(b, n);
+    string sa = convert(a, n), sb = convert(b, n);
     if (sa == sb) {
         cout << "YES " << sa << "\n";
     } else {
--- 1.algorithm/6.stl/3.string/A1060-2.cpp
+++ 1.algorithm/6.stl/3.string/A1060-2.cpp
@@ -7,27 +7,27 @@
  */
 
 /*
-  @pintia psid=994805342720868352 pid=994805413520719872 compiler=GXX
-  ProblemSet: PAT (Advanced Level) Practice
-  Title: 1060 Are They Equal
-  https://pintia.cn/problem-sets/994805342720868352/exam/problems/type/7?problemSetProblemId=994805413520719872
+    @pintia psid=994805342720868352 pid=994805413520719872 compiler=GXX
+    ProblemSet: PAT (Advanced Level) Practice
+    Title: 1060 Are They Equal
+    https://pintia.cn/problem-sets/994805342720868352/exam/problems/type/7?problemSetProblemId=994805413520719872
 */
 
 // @pintia code=start
 // Not Accepted only by test 5, due to the precision of the 99-digits long float number
 #include <iomanip>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
 string trans(string s, int n) {
-    int e = stoi(s.substr(n + 3));
-    string t = s.substr(0, n + 1);
-    t.erase(1, 1);
-    if (e != 0 || stoi(t) != 0) {
-        ++e;
+    int e = stoi(s.substr(n + 3)); // exponent
+    string num = s.substr(0, n + 1).erase(1, 1);
+    if (e != 0 || stoi(num) != 0) {
+        e++; // format for 1.234e+03 => 0.123e+04
     }
-    return "0." + t + "*10^" + to_string(e);
+    return "0." + num + "*10^" + to_string(e);
 }
 
 int main(int argc, char const *argv[]) {
@@ -39,7 +39,7 @@ int main(int argc, char const *argv[]) {
     string b[2];
     for (int i = 0; i < 2; i++) {
         s[i] << scientific << setprecision(n) << a[i];
-        // i.e., 1.234e+03, not 0.123e+03
+        // i.e., 1.234e+03, not 0.123e+04
         b[i] = trans(s[i].str(), n);
     }
     if (b[0] == b[1]) {
