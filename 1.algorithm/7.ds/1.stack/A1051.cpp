84d0cd1bd93c3583e3307320b4550656ba5323de
zhouyuhao <zhouyuhaoqin@qq.com>
2025-04-15 15:57:42 +0800

    DS - Stack

--- 1.algorithm/7.ds/1.stack/A1051.cpp
+++ 1.algorithm/7.ds/1.stack/A1051.cpp
@@ -7,10 +7,10 @@
  */
 
 /*
-  @pintia psid=994805342720868352 pid=994805427332562944 compiler=GXX
-  ProblemSet: PAT (Advanced Level) Practice
-  Title: 1051 Pop Sequence
-  https://pintia.cn/problem-sets/994805342720868352/exam/problems/type/7?problemSetProblemId=994805427332562944
+    @pintia psid=994805342720868352 pid=994805427332562944 compiler=GXX
+    ProblemSet: PAT (Advanced Level) Practice
+    Title: 1051 Pop Sequence
+    https://pintia.cn/problem-sets/994805342720868352/exam/problems/type/7?problemSetProblemId=994805427332562944
 */
 
 // @pintia code=start
@@ -25,27 +25,21 @@ int main(int argc, char const *argv[]) {
     int m, n, k;
     cin >> m >> n >> k;
     for (int c = 0; c < k; c++) {
-        vector<int> d(n);
         queue<int> q;
         for (int i = 0; i < n; i++) {
-            d[i] = i + 1;
             int num;
             cin >> num;
             q.emplace(num);
         }
         stack<int> s;
         for (int i = 0; i < n; i++) {
-            s.emplace(d[i]);
+            s.emplace(i + 1);
             if ((int)s.size() > m) break;
             while (!s.empty() && s.top() == q.front()) {
                 s.pop(), q.pop();
             }
         }
-        if (!s.empty()) {
-            cout << "NO\n";
-        } else {
-            cout << "YES\n";
-        }
+        s.empty() ? cout << "YES\n" : cout << "NO\n";
     }
 
     return 0;
