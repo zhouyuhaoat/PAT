4177bb3168f9bfc169c530679839211924d216ea
zhouyuhao <zhouyuhaoqin@qq.com>
2025-04-16 13:13:06 +0800

    Tree - AVL

--- 1.algorithm/9.tree/5.avl/A1066.cpp
+++ 1.algorithm/9.tree/5.avl/A1066.cpp
@@ -7,10 +7,10 @@
  */
 
 /*
-  @pintia psid=994805342720868352 pid=994805404939173888 compiler=GXX
-  ProblemSet: PAT (Advanced Level) Practice
-  Title: 1066 Root of AVL Tree
-  https://pintia.cn/problem-sets/994805342720868352/exam/problems/type/7?problemSetProblemId=994805404939173888
+    @pintia psid=994805342720868352 pid=994805404939173888 compiler=GXX
+    ProblemSet: PAT (Advanced Level) Practice
+    Title: 1066 Root of AVL Tree
+    https://pintia.cn/problem-sets/994805342720868352/exam/problems/type/7?problemSetProblemId=994805404939173888
 */
 
 // @pintia code=start
